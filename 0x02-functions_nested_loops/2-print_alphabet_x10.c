b4e36658df873ad3d1cc3d4a814b8ebc7e878e24
Abdellatif Oudat <oudat.abdellatif@gmail.com>
2023-06-20 16:10:07 -0700

    m

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ *
+ * Return : void
+ */
+void print_alphabet_x10(void)
+{
+	char txt = 'a';
+
+	int j = 0;
+
+	for (i = 0; i <= 9; i++)
+	{
+		while(txt <= 'z')
+		{
+			_putchar(txt);
+
+			txt++;
+		}
+		_putchar('\n);
+	}
+}
