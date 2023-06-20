7eb5ccdf408fb2c7313860cbfc3bb7df5107aca7
Abdellatif Oudat <oudat.abdellatif@gmail.com>
2023-06-20 15:30:32 -0700

    m

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return : always 0
+ */
+void  print_alphabet(void)
+{
+
+	char txt = 'a';
+
+	while (txt <= 'z' )
+	{
+
+		_putchar(txt);
+
+		txt++;
+	}
+	_putchar('\n')
+
+}
