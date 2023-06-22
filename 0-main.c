876afefb9cbda54a6951b5ccd0ee978932113cf5
Abdellatif Oudat <oudat.abdellatif@gmail.com>
2023-06-21 23:30:55 -0700

    m

---
+++ 0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - test if the integer is positive or negative
+ *
+ * Return: return void
+ */
+int main(void)
+{
+	int i = 0;
+
+	postitive_or_negative(i);
+
+	return (0);
+}
+	
+
