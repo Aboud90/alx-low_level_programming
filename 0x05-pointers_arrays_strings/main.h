a83aef25fd60b921b9b65628a659580c1fe13f5f
Abdellatif Oudat <89777569+Aboud90@users.noreply.github.com>
2023-06-27 09:48:34 +0100

    Update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,7 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
 /**
-*File:main.h
-*Author:Aboud90
-*Desc:header file containing functions declaration
+*File: main.h
+*Author: Aboud90
+*Desc: header file containing functions declaration
 */
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -14,3 +17,4 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src)
+#endif /* MAIN_H */
