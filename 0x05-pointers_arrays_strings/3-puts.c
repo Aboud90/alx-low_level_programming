cbde5480d4db00a5833fa0680f7eba8a0b6f3cd0
Abdellatif Oudat <89777569+Aboud90@users.noreply.github.com>
2023-06-27 10:03:34 +0100

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,8 +1,8 @@
 #include "main.h"
-/*
- * _puts - a function that prints a string
- * @str: test
- * seturn: string
+
+/**
+ * _puts - prints a string to stdout
+ * @str: pointer to the string to print
  */
 void _puts(char *str)
 {
