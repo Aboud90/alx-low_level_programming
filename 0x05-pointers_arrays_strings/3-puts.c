0dd12ad891aea195a256e3e02e71a8436a574829
Abdellatif Oudat <oudat.abdellatif@gmail.com>
2023-06-26 17:41:35 -0700

    m

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,18 +1,16 @@
 #include "main.h"
 /**
- * _puts - prints string
- * @str: string
- * Return: void
+ * _puts - a function that prints a string
+ * @str: test
+ * Return: string
  */
 void _puts(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (str[i] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
-		i++;
 	}
-
 	_putchar('\n');
 }
