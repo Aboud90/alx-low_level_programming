f3e938dc4ebe97b5807a420182779ecce98e2287
Abdellatif Oudat <oudat.abdellatif@gmail.com>
2023-06-27 06:57:19 -0700

    m

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,11 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stdio.h>
-/**
-*File: main.h
-*Author: Aboud90
-*Desc: header file containing functions declaration
-*/
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_in(int *a, int *b);
@@ -16,6 +8,5 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
-void print_array(int *a, int n;
+void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
-#endif /* MAIN_H */
