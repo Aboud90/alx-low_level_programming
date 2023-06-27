dbca73a877f4b0273bfd7b25e4a509b1b06fe320
Abdellatif Oudat <oudat.abdellatif@gmail.com>
2023-06-27 01:53:35 -0700

    m

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,7 +8,8 @@
 */
 int _putchar(char c);
 void reset_to_98(int *n);
-void swap_int(int *a, int *b);
+void swap_in(int *a, int *b);
+void _puts(char *str);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
